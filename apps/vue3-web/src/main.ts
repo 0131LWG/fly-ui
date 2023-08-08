6f5d456d09ee82c6349c9e5bc6e2e8c3cad16681
李文冠 <guan1024z@gmail.com>
2023-08-08 15:02:45 +0800

    feat: 修改名称为v-fly-ui

--- apps/vue3-web/src/main.ts
+++ apps/vue3-web/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import FlyUi from "fly-ui";
-import "fly-ui/dist/index.css";
+import FlyUi from "v-fly-ui";
+import "v-fly-ui/dist/index.css";
 
 createApp(App).use(FlyUi).mount("#app");
