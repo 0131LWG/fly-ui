f9c61ea77f9f686ff4bc54d3c2c698c5a6dd2508
李文冠 <guan1024z@gmail.com>
2023-08-09 09:37:14 +0800

    feat: 修改目录结构,添加ts提示

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,8 +4,6 @@ import dts from "vite-plugin-dts";
 import { presetUno, presetAttributify, presetIcons } from "unocss";
 import Unocss from "unocss/vite";
 import { safelist } from "./config/uno";
-import fs from "fs";
-import path from "path";
 
 export default defineConfig({
   build: {
@@ -16,20 +14,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖，防止多个vue产生冲突。
-      external: ["vue"],
-      plugins: [
-        // 自定义 Rollup 插件，在构建输出目录复制 package.json 文件
-        {
-          name: "copy-package-json",
-          generateBundle(options, bundle) {
-            const packageJsonPath = path.resolve(__dirname, "package.json");
-            const outputDir = options.dir || "dist";
-            const outputFilePath = path.join(outputDir, "package.json");
-
-            fs.copyFileSync(packageJsonPath, outputFilePath);
-          }
-        }
-      ]
+      external: ["vue"]
     },
     cssCodeSplit: true
   },
