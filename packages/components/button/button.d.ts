479264b0d39f11ac36f37553db78801a45bcbdc4
李文冠 <guan1024z@gmail.com>
2023-08-09 09:37:38 +0800

    feat: 修改目录结构,添加ts提示

---
+++ packages/components/button/button.d.ts
@@ -0,0 +1,5 @@
+export type ButtonType = "primary" | "dashed" | "link" | "text" | "default";
+
+export declare type FlyButton = {
+  type: ButtonType;
+};
--- packages/components/button/src/index.vue
+++ packages/components/button/src/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <button>test</button>
+  <button>{{ type }}</button>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+  import { FlyButton } from "../button";
+
+  withDefaults(defineProps<FlyButton>(), {
+    type: "primary"
+  });
+</script>
+
 <style lang="scss" scoped></style>
